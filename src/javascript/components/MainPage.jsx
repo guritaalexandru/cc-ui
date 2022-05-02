cb48012416200f83c96908d64102760e1fb8f443
Alexandru Gurita <alexandru.gurita@flipsidegroup.com>
2022-05-02 20:17:01 +0300

    Styling and functionality

--- src/javascript/components/MainPage.jsx
+++ src/javascript/components/MainPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-function MainPage() {
-    return ( 
-    <div id="MainPage">
+import Header from './Header';
+import MessagesList from './MessagesList';
+import MessagesSubmit from './MessagesSubmit';
 
-    </div>
+function MainPage() {
+    return (
+        <div id="MainPage">
+            <Header />
+                <div className="flex max-w-7xl m-auto px-14 py-24">
+                    <div className='w-1/2 pr-5'>
+                        <MessagesList />
+                    </div>
+                    <div className='w-1/2 pl-5'>
+                        <MessagesSubmit />
+                    </div>
+                </div>
+        </div>
     );
 }
 
